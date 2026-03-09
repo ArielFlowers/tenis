1307d049daad6a6dbff9b741c5865848505bde62
ArielFlowers <arielmakotoflores@gmail.com>
2026-03-09 01:58:25 -0400

    feat: actualizado para el 15,love

--- src/puntaje.js
+++ src/puntaje.js
@@ -1,5 +1,6 @@
 function obtenerPuntaje(p1,p2) {
-    const puntos = ["Love"];
+    const puntos = ["Love","15"];
+    if(p1==1) return "15,Love"
     return `${puntos[p1]},${puntos[p2]}`;
   }
   
--- src/puntaje.test.js
+++ src/puntaje.test.js
@@ -4,5 +4,8 @@ describe("Puntaje de Tenis", () => {
   it("debería empezar en Love,Love", () => {
     expect(obtenerPuntaje(0, 0)).toEqual("Love,Love");
   });
+  it("debería mostrar 15,Love cuando P1 anota un punto", () => {
+    expect(obtenerPuntaje(1, 0)).toEqual("15,Love");
+  });
 
 });
